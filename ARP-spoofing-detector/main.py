3e1c0e28e52d45d1fb4e3bc5e385af1a670c77fd
Mieszko Połonczyk <127117681+M-Polonczyk@users.noreply.github.com>
2023-03-20 18:50:02 +0100

    Update main.py

--- ARP-spoofing-detector/main.py
+++ ARP-spoofing-detector/main.py
@@ -6,13 +6,13 @@ from datetime import datetime
 def arp_table_extractor():
     arp_table = os.popen('arp -a').read()
     arp_table_lines = arp_table.splitlines()
-    addresses = {'MAC': 'IP'}
+    addresses = {'IP': 'MAC'}
     for line in arp_table_lines:
         if line.__contains__('ff:ff:ff:ff:ff:ff') or line.__contains__('ff-ff-ff-ff-ff-ff') or line.__contains__('Int'):
             continue
         if arp_table_lines.index(line) > 0:
             split_line = remove_items(line.split(' '), '')
-            addresses[split_line[1]] = split_line[0]
+            addresses[split_line[0]] = split_line[1]
     return addresses
 
 
@@ -25,11 +25,7 @@ def arp_table_mac_duplication(addr=[]):
 
 
 addresses = arp_table_extractor()
-mac_addr = []
-for row in addresses.keys():
-    mac_addr.append(row)
-print(mac_addr)
-duplicated = arp_table_mac_duplication(mac_addr)
+duplicated = arp_table_mac_duplication([row for row in addresses.values()])
 # test
 # duplicated = arp_table_mac_duplication(['01-5e-5e-01-00-fb', '01-01-5e-00-00-ff', '01-01-5e-00-00-ff']) 
 log_writer(duplicated, datetime.now().strftime('%d/%m/%Y %H:%M:%S')) if duplicated is not False else print('All good!')
