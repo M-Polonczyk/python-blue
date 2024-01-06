058ce17327a8a84b36e38acccfc2764331033c6b
M-Polonczyk <m.polonczyk@outlook.com>
2024-01-06 19:58:08 +0100

    modified:   ARP-spoofing-detector/log.py 	modified:   ARP-spoofing-detector/main.py 	deleted:    socket-localhost.py

--- ARP-spoofing-detector/log.py
+++ ARP-spoofing-detector/log.py
@@ -1,5 +1,12 @@
-def log_writer(mac_address, date):
-    print('Arp spoofed\nGenerating log in log.txt for address',mac_address)
-    with open('log.txt', 'a') as f:
-        f.write('Arp Spoofed!\n'+'The address is: '+mac_address+'\nDate: '+str(date)+'\n\n')
+def log_writer(mac_address: str, date: str) -> None:
+    print("Arp spoofed\nGenerating log in log.txt for address", mac_address)
+    with open("log.txt", "a", encoding="utf-8") as f:
+        f.write(
+            "Arp Spoofed!\n"
+            + "The address is: "
+            + mac_address
+            + "\nDate: "
+            + str(date)
+            + "\n\n"
+        )
         f.close()
--- ARP-spoofing-detector/main.py
+++ ARP-spoofing-detector/main.py
@@ -1,22 +1,23 @@
 import os
+from typing import Dict, List
 from log import log_writer
 from datetime import datetime
 
 
-def arp_table_extractor():
-    arp_table = os.popen('arp -a').read()
+def arp_table_extractor() -> Dict[str, str]:
+    arp_table = os.popen("arp -a").read()
     arp_table_lines = arp_table.splitlines()
-    addresses = {'IP': 'MAC'}
+    addresses = {"IP": "MAC"}
     for line in arp_table_lines:
-        if line.__contains__('ff:ff:ff:ff:ff:ff') or line.__contains__('ff-ff-ff-ff-ff-ff') or line.__contains__('Int'):
+        if "ff:ff:ff:ff:ff:ff" in line or "ff-ff-ff-ff-ff-ff" in line or "Int" in line:
             continue
         if arp_table_lines.index(line) > 0:
-            split_line = remove_items(line.split(' '), '')
+            split_line = line.split(" ")
             addresses[split_line[0]] = split_line[1]
     return addresses
 
 
-def arp_table_mac_duplication(addr=[]):
+def arp_table_mac_duplication(addr: List[str]):
     addr.sort()
     for i in range(len(addr) - 1):
         if addr[i] == addr[i + 1]:
@@ -27,5 +28,7 @@ def arp_table_mac_duplication(addr=[]):
 addresses = arp_table_extractor()
 duplicated = arp_table_mac_duplication([row for row in addresses.values()])
 # test
-# duplicated = arp_table_mac_duplication(['01-5e-5e-01-00-fb', '01-01-5e-00-00-ff', '01-01-5e-00-00-ff']) 
-log_writer(duplicated, datetime.now().strftime('%d/%m/%Y %H:%M:%S')) if duplicated is not False else print('All good!')
+# duplicated = arp_table_mac_duplication(['01-5e-5e-01-00-fb', '01-01-5e-00-00-ff', '01-01-5e-00-00-ff'])
+log_writer(
+    duplicated, datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+) if duplicated is not False else print("All good!")
